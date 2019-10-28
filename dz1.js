b50afddf04837138a8e85fb6cb095719ccf7bbd4
Pedan <antonpedan@MacBook-Air-Anton.local>
2019-10-28 22:58:59 +0200

    <покорно и с благодарностью внес предложенные Яриком изменения>

--- dz1.js
+++ dz1.js
@@ -1,19 +1,20 @@
-let student1 = {
-    name: 'a',
-    height: 166,
-  };
-  console.log(student1.valueOf())
-let student2 = {
-    name: 'b',
-    height: 177,
-  };
-  console.log(student2.valueOf())
-let student3 = {
-    name: 'c',
-    height: 180,
-  };
-  console.log(student3.valueOf())
-  
+/* eslint-disable no-console */
+const student1 = {
+  name: 'a',
+  height: 166,
+};
+console.log(student1);
+const student2 = {
+  name: 'b',
+  height: 177,
+};
+console.log(student2);
+const student3 = {
+  name: 'c',
+  height: 180,
+};
+console.log(student3);
+
 let e = [student1, student2, student3];
-e = e.map(a => a.height).reduce((x,y) => x + y/e.length, 0);
+e = e.map((a) => a.height).reduce((x, y) => x + y / e.length, 0);
 console.log(`Средний рост студента:${Math.round(e)}`);
