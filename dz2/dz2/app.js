d78709fea512ef51bec718ffc707d370659c2b8b
Anton <54960851+pedananton@users.noreply.github.com>
2019-11-01 20:36:15 +0200

    Исправил , неунимательно , формулу не ту всунул :)

---
+++ dz2/dz2/app.js
@@ -0,0 +1,13 @@
+
+'use strict';
+
+const r = 3;
+const h = 9;
+let area;
+let volume;
+
+alert(
+  'площадь' + ' ' + (area = (Math.PI * Math.pow(r, 2))),
+  alert('объем' + ' ' + (volume = (Math.PI * Math.pow(r, 2) * h))),
+);
+
