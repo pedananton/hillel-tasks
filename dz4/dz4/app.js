0912f21d955b390544fdda57ed5855446e67c8e0
Anton <54960851+pedananton@users.noreply.github.com>
2019-11-16 11:44:22 +0200

    Ну как я пока это могу видеть
    обесчаю стараться смотреть глубже :)

---
+++ dz4/dz4/app.js
@@ -0,0 +1,27 @@
+function max2() {
+  if (inputA.value > inputB.value) {
+    inputResult.value = inputA.value;
+  } else {
+    inputResult.value = inputB.value;
+  }
+}
+
+function min2() {
+  if (inputA.value < inputB.value) {
+    inputResult.value = inputA.value;
+  } else {
+    inputResult.value = inputB.value;
+  }
+}
+
+function selectFn() {
+  if (document.getElementById('selectFn').selectedIndex === 0) {
+    max2();
+  } else {
+    min2();
+  }
+}
+
+function result() {
+  selectFn();
+}
