38d8fac44dd0938125a11ded786b075f0b63497f
Anton <54960851+pedananton@users.noreply.github.com>
2019-11-18 19:12:39 +0200

    осознал важность ООП (буду развивать) :)
    добавил объект и обратился к нему из html по свойству

--- dz4/dz4/app.js
+++ dz4/dz4/app.js
@@ -1,27 +1,29 @@
-function max2() {
-  if (inputA.value > inputB.value) {
-    inputResult.value = inputA.value;
-  } else {
-    inputResult.value = inputB.value;
-  }
-}
 
-function min2() {
-  if (inputA.value < inputB.value) {
-    inputResult.value = inputA.value;
-  } else {
-    inputResult.value = inputB.value;
-  }
-}
+const CALC = {
+  amount: function result() {
+    function max2() {
+      if (inputA.value > inputB.value) {
+        inputResult.value = inputA.value;
+      } else {
+        inputResult.value = inputB.value;
+      }
+    }
 
-function selectFn() {
-  if (document.getElementById('selectFn').selectedIndex === 0) {
-    max2();
-  } else {
-    min2();
-  }
-}
+    function min2() {
+      if (inputA.value < inputB.value) {
+        inputResult.value = inputA.value;
+      } else {
+        inputResult.value = inputB.value;
+      }
+    }
 
-function result() {
-  selectFn();
-}
+    function selectFn() {
+      if (document.getElementById('selectFn').selectedIndex === 0) {
+        max2();
+      } else {
+        min2();
+      }
+    }
+    selectFn();
+  },
+};
