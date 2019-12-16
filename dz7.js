acc8491b1b6af58b6fbe2d36da94da0c455ac8dc
Pedan Anton <54960851+pedananton@users.noreply.github.com>
2019-12-16 19:02:07 +0200

    добавил вывод в консоль
    запусти все ж работает )

--- dz7.js
+++ dz7.js
@@ -2,10 +2,11 @@ let arr = [];
 function times(x) {
   for (i = 0; i < x; i++) {
     arr = Array.from({ length: x }, (v, k) => k);
-    if (x = 4) {
-    arr === Array.from({ length: x }, (v, k) => k * k);
+    if (x === 4) {
+    arr = Array.from({ length: x }, (v, k) => k * k);
     }
   }
+  console.log(arr)
 }
 times();
 times(5);
