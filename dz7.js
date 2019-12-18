59b6167becf87b5d26d77da1b4b746d5a3e9c96e
Pedan Anton <54960851+pedananton@users.noreply.github.com>
2019-12-18 19:16:08 +0200

    Update dz7.js

--- dz7.js
+++ dz7.js
@@ -1,13 +1,16 @@
-let arr = [];
-function times(x) {
-  for (i = 0; i < x; i++) {
-    arr = Array.from({ length: x }, (v, k) => k);
-    if (x === 4) {
-    arr = Array.from({ length: x }, (v, k) => k * k);
+function times(x, callback) { 
+  let arr = [];
+  for (var i = 0; i < x; i++) {
+    if(typeof(callback) == "function" ){
+      arr[i] = callback(i);
+    } else { 
+      arr = Array.from({ length: x }, (v, k) => k);
     }
   }
   console.log(arr)
 }
 times();
 times(5);
-times(4);
+times(4, function(i){
+  return i * i;
+}); 
